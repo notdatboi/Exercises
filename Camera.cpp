be4a2890e74702f5df71cace13400db4b5bdb1d3
notdatboi <noddatboi@gmail.com>
2019-06-27 17:01:00 +0300

    Added Camera class

---
+++ Camera.cpp
@@ -0,0 +1,53 @@
+#include"Camera.hpp"
+
+Camera::Camera(): position({0, 0, 0}), yaw(0), pitch(0) {}
+
+Camera::Camera(const glm::vec3 initialPosition, const float initialYaw, const float initialPitch): position(initialPosition), yaw(initialYaw), pitch(initialPitch) {}
+
+Camera& Camera::rotate(const float yaw, const float pitch)
+{
+    this->yaw = yaw;
+    this->pitch = pitch;
+    if(this->pitch < -89.0) this->pitch = -89.0;
+    if(this->pitch > 89.0) this->pitch = 89.0;
+
+    return *this;
+}
+
+Camera& Camera::move(const float shiftX, const float shiftY, const float shiftZ)
+{
+    position.x += shiftX;
+    position.y += shiftY;
+    position.z += shiftZ;
+
+    return *this;
+}
+
+Camera& Camera::setPosition(const glm::vec3 position)
+{
+    this->position = position;
+
+    return *this;
+}
+
+Camera& Camera::setRotation(const float yaw, const float pitch)
+{
+    this->yaw = yaw;
+    this->pitch = pitch;
+
+    return *this;
+}
+
+const glm::vec3 Camera::getPosition() const
+{
+    return position;
+}
+
+const glm::vec3 Camera::getNormalizedDirection() const
+{
+    glm::vec3 direction;
+    direction.x = glm::cos(glm::radians(pitch)) * glm::cos(glm::radians(yaw));
+    direction.y = -glm::cos(glm::radians(pitch)) * glm::sin(glm::radians(yaw));
+    direction.z = -glm::sin(glm::radians(pitch));
+    return glm::normalize(direction);
+}
---
+++ Camera.hpp
@@ -0,0 +1,29 @@
+#ifndef CAMERA_HPP
+#define CAMERA_HPP
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+
+
+class Camera
+{
+public:
+    Camera();
+    Camera(const glm::vec3 initialPosition, const float initialYaw, const float initialPitch);
+    Camera& rotate(const float yaw, const float pitch);
+    Camera& move(const float shiftX, const float shiftY, const float shiftZ);
+    Camera& setPosition(const glm::vec3 position);
+    Camera& setRotation(const float yaw, const float pitch);
+
+    const glm::vec3 getPosition() const;
+    const glm::vec3 getNormalizedDirection() const;
+private:
+    glm::vec3 position;
+    float yaw;
+    float pitch;
+};
+
+#endif
