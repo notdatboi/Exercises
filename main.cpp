871f6bae51bb0350d3ed234c7f5dd47847ffe825
notdatboi <noddatboi@gmail.com>
2019-07-06 20:07:21 +0300

    Rewrote main.cpp to suit new Application class

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
-#include"App.hpp"
+#include"Application.hpp"
 
 int main()
 {
-    spk::system::init();
-    App* app = new App();
+    glfwInit();
+    std::vector<const char*> glfwExtensions;
+    uint32_t count = 0;
+    glfwGetRequiredInstanceExtensions(&count);
+    glfwExtensions.resize(count);
+    glfwExtensions.data = glfwGetRequiredInstanceExtensions(&count);
+    spk::system::init(glfwExtensions);
+    Application* app = new Application();
     app->run();
     delete app;
     spk::system::deinit();
+    glfwTerminate();
 }
