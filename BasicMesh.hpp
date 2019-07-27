7afa8df1cbe32ec97394f034cf2b66aed3dfdc6a
notdatboi <noddatboi@gmail.com>
2019-07-27 17:43:34 +0300

    Introduced Mesh overload for meshes that will be used inside the example app

---
+++ BasicMesh.hpp
@@ -0,0 +1,12 @@
+#ifndef BASIC_MESH_HPP
+#define BASIC_MESH_HPP
+#include"Mesh.hpp"
+
+class BasicMesh : public Mesh
+{
+public:
+    virtual void createPipeline(const uint32_t pipelineIndex, const std::vector<spk::ShaderInfo> shaderInfos, const vk::Extent2D extent, const spk::AdditionalInfo& info);
+private:
+};
+
+#endif
