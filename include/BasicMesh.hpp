6eb86739007015f99a06a9245ae0ccae42061a7f
notdatboi <noddatboi@gmail.com>
2019-07-30 15:02:39 +0300

    Added template Vertex parameter

--- include/BasicMesh.hpp
+++ include/BasicMesh.hpp
@@ -2,11 +2,89 @@
 #define BASIC_MESH_HPP
 #include"Mesh.hpp"
 
-class BasicMesh : public Mesh
+template<typename Vertex>
+class BasicMesh : public Mesh<Vertex>
 {
 public:
-    virtual void createPipeline(const uint32_t pipelineIndex, const std::vector<spk::ShaderInfo> shaderInfos, const vk::Extent2D extent, const spk::AdditionalInfo& info);
+    virtual void createPipeline(const uint32_t pipelineIndex, const VertexDescription& vertexDescription, const std::vector<spk::ShaderInfo> shaderInfos, const vk::Extent2D extent, const spk::AdditionalInfo& info);
 private:
 };
 
+template<typename Vertex>
+void BasicMesh<Vertex>::createPipeline(const uint32_t pipelineIndex, const VertexDescription& vertexDescription, const std::vector<spk::ShaderInfo> shaderInfos, const vk::Extent2D extent, const spk::AdditionalInfo& info)
+{
+    if(pipelineIndex >= this->shaderSets.size())
+    {
+        throw std::invalid_argument("Pipeline index is out of range.\n");
+    }
+    this->loadShaders(pipelineIndex, shaderInfos);
+
+    spk::ShaderStages shaderStages;
+    shaderStages.stages = this->shaderSets[pipelineIndex].getShaderStages();
+
+    vk::VertexInputBindingDescription bindingDesc;
+    bindingDesc.setBinding(0)
+        .setInputRate(vk::VertexInputRate::eVertex)
+        .setStride(sizeof(Vertex));
+
+    spk::VertexInputState vertexInputState;
+    vertexInputState.bindingDescriptions = {bindingDesc};
+    vertexInputState.attributeDescriptions = vertexDescription.getAttributeDescriptions();
+
+    spk::InputAssemblyState assemblyState;
+    assemblyState.enablePrimitiveRestart = false;
+    assemblyState.topology = vk::PrimitiveTopology::ePatchList;
+
+    vk::Rect2D scissor;
+    scissor.setOffset({0, 0})
+        .setExtent({extent.width, extent.height});
+
+    vk::Viewport viewport;
+    viewport.setX(0)
+        .setY(0)
+        .setWidth(extent.width)
+        .setHeight(extent.height)
+        .setMinDepth(0.0f)
+        .setMaxDepth(1.0f);
+
+    spk::TessellationState tessellationState;
+    tessellationState.patchControlPointCount = 4;
+
+    spk::ViewportState viewportState;
+    viewportState.scissor = scissor;
+    viewportState.viewport = viewport;
+
+    spk::RasterizationState rasterizationState;
+    rasterizationState.cullMode = vk::CullModeFlagBits::eBack;
+    rasterizationState.enableDepthClamp = false;
+    rasterizationState.frontFace = vk::FrontFace::eCounterClockwise;
+
+    spk::MultisampleState multisampleState;
+    multisampleState.rasterizationSampleCount = vk::SampleCountFlagBits::e1;
+
+    spk::DepthStencilState depthStencilState;
+    depthStencilState.enableDepthTest = true;
+    depthStencilState.depthCompareOp = vk::CompareOp::eLess;
+    depthStencilState.writeTestResults = true;
+
+    vk::PipelineColorBlendAttachmentState colorBlendAttachmentState;
+    colorBlendAttachmentState.setBlendEnable(false)
+        .setColorWriteMask(vk::ColorComponentFlagBits::eR | vk::ColorComponentFlagBits::eG | vk::ColorComponentFlagBits::eB | vk::ColorComponentFlagBits::eA);
+    spk::ColorBlendState colorBlendState;
+    colorBlendState.attachmentStates = {colorBlendAttachmentState};
+
+    spk::DynamicState dynamicState;
+
+    this->pipelines[pipelineIndex].create(shaderStages, 
+        vertexInputState,
+        assemblyState,
+        tessellationState,
+        viewportState,
+        rasterizationState,
+        multisampleState,
+        depthStencilState,
+        colorBlendState,
+        dynamicState,
+        info);
+}
 #endif
--- include/Mesh.hpp
+++ include/Mesh.hpp
@@ -4,19 +4,14 @@
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+#include"VertexClasses.hpp"
 #include"../../Vulkan-wrapper/include/Spark.hpp"
 #include<glm/glm.hpp>
 #include<glm/gtc/matrix_transform.hpp>
 #include<vector>
 #include<string>
 
-struct Vertex
-{
-    alignas(16) glm::vec3 position;
-    alignas(16) glm::vec3 normal;
-    alignas(8) glm::vec2 uv;
-};
-
+template<typename Vertex>
 class Mesh                  // abstract mesh class
 {
 public:
@@ -24,7 +19,7 @@ public:
         const std::vector<uint32_t>& indices, 
         const std::vector<vk::DescriptorSet>& descriptorSets,
         const uint32_t pipelineCount);
-    virtual void createPipeline(const uint32_t pipelineIndex, const std::vector<spk::ShaderInfo> shaderInfos, const vk::Extent2D extent, const spk::AdditionalInfo& info) = 0;
+    virtual void createPipeline(const uint32_t pipelineIndex, const VertexDescription& vertexDescription, const std::vector<spk::ShaderInfo> shaderInfos, const vk::Extent2D extent, const spk::AdditionalInfo& info) = 0;
     const Mesh& bindVertexBuffer(spk::Subpass& subpass) const;
     const Mesh& bindIndexBuffer(spk::Subpass& subpass) const;
     const Mesh& bindPipeline(spk::Subpass& subpass, const uint32_t index) const;
@@ -49,5 +44,162 @@ protected:
     spk::Buffer indexBuffer;
     uint32_t indexCount;
 };
+
+template<typename Vertex>
+void Mesh<Vertex>::create(const std::vector<Vertex>& vertices, 
+    const std::vector<uint32_t>& indices, 
+    const std::vector<vk::DescriptorSet>& descriptorSets,
+    const uint32_t pipelineCount)
+{
+    const auto& logicalDevice = spk::system::System::getInstance()->getLogicalDevice();
+
+    this->descriptorSets = descriptorSets;
+    pipelines.resize(pipelineCount);
+    shaderSets.resize(pipelineCount);
+
+    vk::FenceCreateInfo fenceInfo;
+    if(logicalDevice.createFence(&fenceInfo, nullptr, bufferUpdateFences) != vk::Result::eSuccess) throw std::runtime_error("Failed to create fence!\n");
+    if(logicalDevice.createFence(&fenceInfo, nullptr, bufferUpdateFences + 1) != vk::Result::eSuccess) throw std::runtime_error("Failed to create fence!\n");
+    
+    writeBuffers(vertices, indices);
+}
+
+template<typename Vertex>
+void Mesh<Vertex>::writeBuffers(const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indices)
+{
+    const auto& pool = spk::system::Executives::getInstance()->getPool();
+    const auto& logicalDevice = spk::system::System::getInstance()->getLogicalDevice();
+
+    vertexCount = vertices.size();
+    indexCount = indices.size();
+
+    vk::CommandBufferAllocateInfo CBAllocInfo;
+    CBAllocInfo.setCommandBufferCount(2)
+        .setCommandPool(pool)
+        .setLevel(vk::CommandBufferLevel::ePrimary);
+    if(logicalDevice.allocateCommandBuffers(&CBAllocInfo, bufferUpdateCBs) != vk::Result::eSuccess) throw std::runtime_error("Failed to allocate command buffer!\n");
+    
+    vertexBuffer.create(vertices.size() * sizeof(Vertex), vk::BufferUsageFlagBits::eTransferDst | vk::BufferUsageFlagBits::eVertexBuffer, true, false);
+    indexBuffer.create(indices.size() * sizeof(uint32_t), vk::BufferUsageFlagBits::eTransferDst | vk::BufferUsageFlagBits::eIndexBuffer, true, false);
+    vertexBuffer.bindMemory();
+    indexBuffer.bindMemory();
+
+    spk::Buffer vertexBufferOnCPU, indexBufferOnCPU;
+    vertexBufferOnCPU.create(vertices.size() * sizeof(Vertex), vk::BufferUsageFlagBits::eTransferSrc, false, false);
+    indexBufferOnCPU.create(indices.size() * sizeof(uint32_t), vk::BufferUsageFlagBits::eTransferSrc, false, false);
+    vertexBufferOnCPU.bindMemory();
+    indexBufferOnCPU.bindMemory();
+    vertexBufferOnCPU.updateCPUAccessible(reinterpret_cast<const void *>(vertices.data()));
+    indexBufferOnCPU.updateCPUAccessible(reinterpret_cast<const void *>(indices.data()));
+
+    vertexBuffer.updateDeviceLocal(bufferUpdateCBs[0], 
+        vertexBufferOnCPU.getBuffer(),
+        0,
+        vk::Semaphore(),
+        vk::Semaphore(),
+        vk::Fence(),
+        bufferUpdateFences[0],
+        vk::PipelineStageFlagBits::eTopOfPipe,
+        true);
+    indexBuffer.updateDeviceLocal(bufferUpdateCBs[1],
+        indexBufferOnCPU.getBuffer(),
+        0,
+        vk::Semaphore(),
+        vk::Semaphore(),
+        vk::Fence(),
+        bufferUpdateFences[1],
+        vk::PipelineStageFlagBits::eTopOfPipe,
+        true);
+    
+    if(logicalDevice.waitForFences(2, bufferUpdateFences, true, ~0U) != vk::Result::eSuccess) throw std::runtime_error("Failed to wait for fences!\n");
+
+    vertexBufferOnCPU.destroy();
+    indexBufferOnCPU.destroy();
+}
+
+template<typename Vertex>
+const Mesh<Vertex>& Mesh<Vertex>::bindVertexBuffer(spk::Subpass& subpass) const
+{
+    std::vector<vk::Buffer> vertexBuffers;
+    vertexBuffers.push_back(vertexBuffer.getBuffer());
+    subpass.bindVertexBuffers(vertexBuffers);
+
+    return *this;
+}
+
+template<typename Vertex>
+const Mesh<Vertex>& Mesh<Vertex>::bindIndexBuffer(spk::Subpass& subpass) const
+{
+    subpass.bindIndexBuffer(indexBuffer.getBuffer(), vk::IndexType::eUint32);
+
+    return *this;
+}
+
+template<typename Vertex>
+const Mesh<Vertex>& Mesh<Vertex>::bindPipeline(spk::Subpass& subpass, const uint32_t index) const
+{
+    subpass.bindPipeline(pipelines[index].getPipeline());
+
+    return *this;
+}
+
+template<typename Vertex>
+const Mesh<Vertex>& Mesh<Vertex>::bindDescriptorSets(spk::Subpass& subpass, const uint32_t pipelineIndex, const uint32_t firstSetToUpdate) const
+{
+    subpass.bindDescriptorSets(pipelines[pipelineIndex].getLayout(), descriptorSets, firstSetToUpdate);
+
+    return *this;
+}
+
+template<typename Vertex>
+const Mesh<Vertex>& Mesh<Vertex>::draw(spk::Subpass& subpass, const uint32_t instanceCount, const uint32_t firstInstance) const
+{
+    subpass.draw(vertexCount, instanceCount, 0, firstInstance);
+
+    return *this;
+}
+
+template<typename Vertex>
+const Mesh<Vertex>& Mesh<Vertex>::drawIndexed(spk::Subpass& subpass, const uint32_t instanceCount, const uint32_t firstInstance) const
+{
+    if(indexCount == 0) throw std::runtime_error("There are no indices to draw.\n");
+    subpass.drawIndexed(indexCount, instanceCount, 0, firstInstance);
+
+    return *this;
+}
+
+template<typename Vertex>
+void Mesh<Vertex>::loadShaders(const uint32_t shaderSetIndex, const std::vector<spk::ShaderInfo> shaderInfos)
+{
+    if(shaderSetIndex >= shaderSets.size())
+    {
+        throw std::invalid_argument("Shader set index is out of range.\n");
+    }
+    shaderSets[shaderSetIndex].create(shaderInfos);
+}
+
+template<typename Vertex>
+void Mesh<Vertex>::destroy()
+{
+    const auto& logicalDevice = spk::system::System::getInstance()->getLogicalDevice();
+    const auto& pool = spk::system::Executives::getInstance()->getPool();
+
+    if(bufferUpdateFences[0])
+    {
+        logicalDevice.destroyFence(bufferUpdateFences[0], nullptr);
+        logicalDevice.destroyFence(bufferUpdateFences[1], nullptr);
+        bufferUpdateFences[0] = bufferUpdateFences[1] = vk::Fence();
+    }
+    if(bufferUpdateCBs[0])
+    {
+        logicalDevice.freeCommandBuffers(pool, 2, bufferUpdateCBs);
+    }
+}
+    
+template<typename Vertex>
+Mesh<Vertex>::~Mesh<Vertex>()
+{
+    destroy();
+}
 
 #endif
